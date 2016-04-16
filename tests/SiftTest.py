e882f847215a40b50d306110e428eb6509ddd3ca
jaramosr <jaramosr@ncsu.edu>
2016-04-16 15:48:53 -0400

    added PCA test to the sift test file

--- tests/SiftTest.py
+++ tests/SiftTest.py
@@ -6,20 +6,26 @@ from sklearn.decomposition import PCA
 if __name__ == '__main__':
     print("This is just a test!\n")
 
-    imgName = '../images/raleigh/small_image.png'
+    imgName1 = '../images/Classified/Nature/slice_19-26.png'
+    imgName2 = '../images/Classified/Nature/slice_19-27.png'
 
     # this image takes forever to compute - try way smaller please
 
-    vlfeat.process_image(imgName, 'tmp.sift')
-    l, d = vlfeat.read_features_from_file('tmp.sift')
+    vlfeat.process_image(imgName1, 'tmp.sift1')
+    l, d = vlfeat.read_features_from_file('tmp.sift1')
+
+    vlfeat.process_image(imgName2, 'tmp.sift2')
+    l2, d2 = vlfeat.read_features_from_file('tmp.sift2')
 
     # im = array(Image.open(imgName))
     # figure()
     # vlfeat.plot_features(im, l, True)
     # show()
 
-    print d
-
-    pca = PCA(n_components=2)
+    pca = PCA(n_components=3)
     pca.fit(d)
+    print(pca.explained_variance_ratio_)
+
+    pca = PCA(n_components=3)
+    pca.fit(d2)
     print(pca.explained_variance_ratio_)
